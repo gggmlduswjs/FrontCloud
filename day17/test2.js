5253de2fd24c46df98ff8ae9d0db23cc74cd2c9d
gggmlduswjs <sjrnf5749@naver.com>
2025-08-05 17:42:57 +0900

    dayw17

---
+++ day17/test2.js
@@ -0,0 +1,9 @@
+
+//dom이 구축이 완료된 후
+document.addEventListener("DOMContentLoaded",function(){
+    const title = document.getElementById("title");
+    title.textContent="title ^^";
+});
+
+// dom을 포함한 모든 리소스 완료된 후 
+// window.addEventListener("load" ,function(){});
